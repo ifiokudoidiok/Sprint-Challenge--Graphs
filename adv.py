f8c7ce2ae5e86ef7107b51417ce91f7619566935
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-03-19 20:55:33 +0100

    reverse direction set

--- adv.py
+++ adv.py
@@ -108,8 +108,17 @@ unexplored_room = {}
 for direction in player.current_room.get_exits():
     #add all ? exits to unexplored_room
     unexplored_room[direction] = "?"
-    #the starting room should always be an unexplored room
+    #the starting room should be an unexplored room
 map[world.starting_room.id] = unexplored_room
+
+#turn new_moves into a queue
+new_moves = Queue()
+#call untried 
+untried(player, new_moves)
+
+#set the reverse directions, just like in the adventure game
+reverse_dir = {"n": "s", "s": "n", "e": "w", "w": "e"}
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
